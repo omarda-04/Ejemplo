363c41567731d99aa580046e6b3ab3c9f9a02aba
omarda-04 <omarda1406@gmail.com>
2024-03-21 18:25:28 -0500

    Commit de master

---
+++ src/main/java/co/ogaitan/appInsumos/AppInsumosApplication.java
@@ -0,0 +1,13 @@
+package co.ogaitan.appInsumos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInsumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInsumosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ogaitan/appInsumos/AppInsumosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ogaitan.appInsumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInsumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
